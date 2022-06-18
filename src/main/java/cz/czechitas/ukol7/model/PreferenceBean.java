8effc209ffad06be4e3677553cce233302bd42eb
irenabartunkova <irena.bartunkova@gmail.com>
2022-06-18 10:19:29 +0200

    Úkol hotovo

---
+++ src/main/java/cz/czechitas/ukol7/model/PreferenceBean.java
@@ -0,0 +1,41 @@
+package cz.czechitas.ukol7.model;
+
+import com.jgoodies.common.bean.ObservableBean;
+import com.jgoodies.binding.beans.ExtendedPropertyChangeSupport;
+
+import java.beans.PropertyChangeListener;
+
+public class PreferenceBean {
+    private final ExtendedPropertyChangeSupport pcs = new ExtendedPropertyChangeSupport(this);
+    private String prezdivka;
+    private Barva barva;
+
+    public String getPrezdivka() {
+        return prezdivka;
+    }
+
+    public void setPrezdivka(String prezdivka) {
+        String oldValue=this.prezdivka;
+        this.prezdivka = prezdivka;
+        pcs.firePropertyChange("prezdivka", oldValue, prezdivka);
+    }
+
+    public Barva getBarva() {
+        return barva;
+    }
+
+    public void setBarva(Barva barva) {
+        Barva oldValue=this.barva;
+        this.barva = barva;
+        pcs.firePropertyChange("barva", oldValue, barva);
+    }
+
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        pcs.removePropertyChangeListener(listener);
+    }
+}
